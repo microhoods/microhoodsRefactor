43306af41509fbaa8a5d3ea83546c7eedc608163
Kia Fathi <kiafathi@me.com>
2014-07-23 15:27:08 -0700

    Implement gulp fix

--- gulpfile.js
+++ gulpfile.js
@@ -2,3 +2,18 @@ var gulp = require('gulp'),
     mocha = require('gulp-mocha');
     livereload = require('gulp-livereload'),
     notify = require('gulp-notify');
+
+// gulp.task('watch', function(){
+
+//     var server = livereload();
+//     //Watch server files
+//     gulp.watch('./server').on('change', function(file){
+//         server.changed(file.path);
+//     });
+// });
+
+
+gulp.task('default', function () {
+    return gulp.src('./spec/routesSpec.js', {read: false})
+        .pipe(mocha({reporter:'nyan'}));
+});
