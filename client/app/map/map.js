abc0da577d12b89f34f76fdd6caf171bb64e54c4
Mason <maseh87@gmail.com>
2014-07-23 13:18:52 -0700

    maps works perfect

--- client/app/map/map.js
+++ client/app/map/map.js
@@ -1,3 +1,11 @@
 angular.module('app.map', [])
 
-.controller('MapController', function($scope) {});
+.controller('MapController', function($scope) {
+  $scope.map = {
+    center: {
+        latitude: 45,
+        longitude: -73
+    },
+    zoom: 8
+  };
+});
