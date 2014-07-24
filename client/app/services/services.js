4d20daa0e6ffab3e128511b80d9082a647c3ed28
Mason <maseh87@gmail.com>
2014-07-23 22:37:48 -0700

    removed resolve property and created a service cache to hold all future plot points

--- client/app/services/services.js
+++ client/app/services/services.js
@@ -1,3 +1,22 @@
 angular.module('app.services', [])
 
-.factory('MapFactory', function() {});
+.factory('MapFactory', function() {
+  var cache = {
+    geolocation: {
+      lat: 37.7836,
+      long: -122.4090
+    }
+  };
+
+
+  return {
+    cache: cache,
+    updateGeoCache: function(lat, long) {
+      cache.geolocation = {
+        lat: lat,
+        long: long
+      };
+      console.log('factory cache => ', cache);
+    }
+  };
+});
