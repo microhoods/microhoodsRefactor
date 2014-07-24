528fabfcfcefc081caaa2a7229f3f8776ab7258a
Mason <maseh87@gmail.com>
2014-07-24 00:22:37 -0700

    ng-repeating over hard coded markers. Also have the ability to get lat and long on any double click of the map

--- client/app/map/map.js
+++ client/app/map/map.js
@@ -10,12 +10,36 @@ angular.module('app.map', [])
     zoom: 16
   };
 
-  $scope.marker = {
-    coords: {
-      latitude: MapFactory.cache.geolocation.lat,
-      longitude: MapFactory.cache.geolocation.long
+  $scope.markers = [
+    {
+      id: 1,
+      coords: {
+        latitude: 37.7836,
+        longitude: -122.4090
+      }
+    },
+    {
+      id: 2,
+      coords: {
+        latitude: 37.786368644391295,
+        longitude:  -122.4082088470459
+      }
+    },
+    {
+      id: 3,
+      coords: {
+        latitude:  37.783901211929845,
+        longitude: -122.41260766983032
+      }
+    },
+    {
+      id: 4,
+      coords: {
+        latitude: 37.78586414170478,
+        longitude: -122.4110466241018
+      }
     }
-  };
+  ];
 
   $scope.circle = {
     center: {
@@ -28,6 +52,18 @@ angular.module('app.map', [])
     },
     stroke: {
       weight: 0.5
+    }
+  };
+
+  $scope.events = {
+    dblclick: function(map, event, args) {
+      // var long = args[0].latLng.B;
+      // var lat = args[0].latLng.K;
+      // $scope.markers.push({
+      //   latitude: lat,
+      //   long: long
+      // });
+      console.log(args[0].latLng);
     }
   };
 
