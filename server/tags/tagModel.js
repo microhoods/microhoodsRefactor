160a3f9995fb64eb45afe7ef2e6debe91118916c
Kia Fathi <kiafathi@me.com>
2014-07-23 18:03:04 -0700

    update tagSchema

--- server/tags/tagModel.js
+++ server/tags/tagModel.js
@@ -3,7 +3,34 @@ var mongoose = require('mongoose'),
 
 
 var tagSchema = new Schema({
-  //Going to have longitude/latitude geo data that is a 2d array of numbers
-  //sentiment which is a string
+
   //userId that references a singleUser
+  user: {
+    type: Schema.Types.ObjectId, 
+    ref: 'user',
+    required: true
+  },
+
+  //sentiment which is a string
+  sentiment: {
+    type: String,
+    required: true
+  },
+
+  //Going to have longitude/latitude geo data that is a 2d array of numbers
+  geo: {
+    type: Number, 
+    index: '2d', 
+    required: true
+  },
+
+  //standard entered_at, for potential future features.
+  entered_at: {
+    type: Date, 
+    default: Date,
+    required: true
+  } 
+
 });
+
+var tag = mongoose.model('tag', tagSchema)
