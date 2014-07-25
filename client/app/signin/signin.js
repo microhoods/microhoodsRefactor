090f90c0ae5e5fc668644ee28086df22cec13166
Mason <maseh87@gmail.com>
2014-07-24 19:11:53 -0700

    removed http request

--- client/app/signin/signin.js
+++ client/app/signin/signin.js
@@ -1,12 +1,4 @@
 angular.module('app.signin', [])
 
 .controller('SigninController', function($scope, $http) {
-  $scope.login = function() {
-    $http({
-      method: 'GET',
-      url: 'microhood.azurewebsites.net/api/auth/google'
-    }).then(function(data) {
-      console.log(data.results);
-    });
-  };
 });
