789c5fbb98cd70fca8ee3dcc6ea3e1ac7268c6ed
Mason <maseh87@gmail.com>
2014-07-25 09:20:09 -0700

    refactored events

--- client/app/map/map.js
+++ client/app/map/map.js
@@ -1,13 +1,38 @@
 angular.module('app.map', [])
 
 .controller('MapController', function($scope, MapFactory, $http) {
-  $scope.quickMarkers = [];
+
   $scope.map = {
     center: {
       latitude: MapFactory.cache.geolocation.lat,
       longitude: MapFactory.cache.geolocation.long
     },
-    zoom: 16
+    zoom: 16,
+    events: {
+      dblclick: function(map, event, args) {
+        var plot = {
+          _id: 1,
+          sentiment: 'Plot',
+          geo: [
+            args[0].latLng.B,
+            args[0].latLng.k
+          ]
+        };
+        MapFactory.postMarkers(plot)
+          .then(function(data) {
+            console.log('this is data posted');
+            console.log(data);
+          $scope.markers.push({
+            coords: {
+            latitude: data.geo[1],
+            longitude: data.geo[0]
+            }
+          });
+        console.log('Markers');
+        console.log($scope.markers);
+        });
+      }
+    }
   };
 
   $scope.markers = [];
@@ -26,34 +51,6 @@ angular.module('app.map', [])
     }
   };
 
-  $scope.events = {
-    dblclick: function(map, event, args) {
-      console.log('Args');
-      console.log(args);
-    var plot = {
-      _id: 1,
-      sentiment: 'Plot',
-      geo: [
-        args[0].latLng.B,
-        args[0].latLng.k
-      ]
-    };
-      MapFactory.postMarkers(plot)
-        .then(function(data) {
-          console.log('this is data posted');
-          console.log(data);
-        $scope.markers.push({
-          coords: {
-          latitude: data.geo[1],
-          longitude: data.geo[0]
-          }
-        });
-        console.log('Markers');
-        console.log($scope.markers);
-      });
-  }
-};
-
   $scope.window = {
     coords: {
       latitude: MapFactory.cache.geolocation.lat,
@@ -63,4 +60,13 @@ angular.module('app.map', [])
     templateUrl: 'app/map/window.html'
   };
 
+  $scope.markerEvents = {
+    dblclick: function(map, event, args) {
+      console.log(args);
+    }
+  };
+
+  $scope.tagDetails = function() {
+    console.log('yo');
+  };
 });
